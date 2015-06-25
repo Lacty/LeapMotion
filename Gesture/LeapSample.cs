ce312f21336cf5e1f9f02ce713fc35f9f72dca6a
U-PS14B22\vantan <akira206@gmail.com>
2015-06-25 17:01:18 +0900

    Gesture追加[Circle認識OK]

---
+++ Gesture/LeapSample.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+
+public class LeapSample : MonoBehaviour {
+
+  Controller controller = new Controller();
+
+  public int FingerCount;
+  [SerializeField]
+  private GameObject[] fingerObj;
+
+  private GestureList gestures;
+
+  void Start() {
+    controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+  }
+
+  void Update() {
+    Frame frame = controller.Frame();
+    gestures = frame.Gestures();
+    FingerCount = frame.Fingers.Count;
+
+    InteractionBox interactionBox = frame.InteractionBox;
+
+    for (int i = 0; i < FingerCount; ++i) {
+      var finger = frame.Fingers[i];
+      var obj = fingerObj[i];
+      Vector normPos = interactionBox.NormalizePoint(finger.TipPosition);
+      normPos *= 10;
+      normPos.z = -normPos.z;
+      obj.transform.localPosition = new UnityEngine.Vector3(normPos.x, normPos.y, normPos.z);
+    }
+    ChangeColor();
+  }
+
+  void ChangeColor() {
+    for (int i = 0; i < FingerCount; ++i) {
+      if (gestures[i].Type == Gesture.GestureType.TYPE_CIRCLE) {
+        var obj = fingerObj[i];
+        obj.transform.Rotate(100, 100, 0);
+      }
+    }
+  }
+}
